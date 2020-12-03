427e9857fb79f124091c416e4416902b5c58d3ca
Alessandro Persiano <alessandro.persiano@rawfish.com>
2020-12-03 17:58:53 +0100

    Add fragment names on textview title

--- app/src/main/java/com/example/spikesolutiondynamicbottomnav/ui/home/HomeFragmentTwo.kt
+++ app/src/main/java/com/example/spikesolutiondynamicbottomnav/ui/home/HomeFragmentTwo.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.spikesolutiondynamicbottomnav.MainActivity
 import com.example.spikesolutiondynamicbottomnav.R
 import com.example.spikesolutiondynamicbottomnav.ui.BaseFragment
 
@@ -24,9 +29,18 @@ class HomeFragmentTwo : BaseFragment() {
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+        textView.text = this::class.java.simpleName
+
+        val editText: EditText = root.findViewById(R.id.editTextTextArg)
+        val button: Button = root.findViewById(R.id.button)
+        button.setOnClickListener {
+            findNavController().navigate(
+                MainActivity.Nav.Dest.homeTwo, bundleOf(
+
+                )
+            )
+        }
+
         return root
     }
 }
